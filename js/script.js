a16cd596e15a11a0ca291c7cf06f53958973400c
Keertz44 <ronaldomanrique23@gmail.com>
2023-10-12 17:19:19 -0500

    ya esta

--- js/script.js
+++ js/script.js
@@ -1,9 +1,37 @@
 const wrapper = document.querySelector('.wrapper');
 
-function activeSignUp() {
-    wrapper.classList.add('activeSignUp');
+function activeSignIn() {
+    wrapper.classList.add('activeSignIn');
+    wrapper.classList.remove('activeSignUp');
 }
 
-console.log('hola mundo');
+function activeSignUp() {
+    wrapper.classList.add('activeSignUp');
+    wrapper.classList.remove('activeSignIn');
+}
+
+function changeColor(color) {
+    const curved = document.getElementById('curved');
+    const bgAnimate = document.getElementById('bg-animate');
+
+    curved.style.background = color;
+    bgAnimate.style.background = color;
+    bgAnimate.classList.add('active');
+
+    setTimeout(function(){
+        bgAnimate.classList.remove('active');
+    },1200);
+}
+
+const color = document.querySelectorAll('.color');
+
+function activeLink() {
+    color.forEach((item) =>
+    item.classList.remove('active'))
+    this.classList.add('active')
+}
+
+color.forEach((item) =>
+item.addEventListener('click',activeLink));
 
 
