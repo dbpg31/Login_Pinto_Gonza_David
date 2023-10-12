2e84c59e3d7660fe169a809147fd243767d75572
Keertz44 <ronaldomanrique23@gmail.com>
2023-10-12 18:05:14 -0500

    05

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyCd9ruRGWZAEek6rb7F4fiMJxza6SM1wCQ",
+   authDomain: "fir-app-2fd0c.firebaseapp.com",
+   projectId: "fir-app-2fd0c",
+   storageBucket: "fir-app-2fd0c.appspot.com",
+   messagingSenderId: "496691783102",
+   appId: "1:496691783102:web:d868b1eb544d54ea3f45ef"
+ };
+
+ // Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
